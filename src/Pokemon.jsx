3cfe345e12be0a87fe2c15d9cb365ab4ba37e0b1
unknown <muktinathrajbanshi786@gmail.com>
2025-09-23 20:49:33 +0545

    wow data is fetch from Pokemon API

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import "./index.css";
+
 
 export const Pokemon = () => {
+
+    const API = "https://pokeapi.co/api/v2/pokemon?limit=24";
+
+    const fetchPokemon = async () => {
+        try {
+           const res = await fetch(API);
+           const data = await res.json(); 
+           console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+useEffect(() => {
+    fetchPokemon();
+}, []);
+
   return (
     <>
-      <h1>Hello, Pokemon re Baba</h1>
+      <h1>Hello, Pokemon</h1>
     </>
   );
 };
