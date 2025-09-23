7e0a42640f3efecd8a0868deb8e899da90c67803
unknown <muktinathrajbanshi786@gmail.com>
2025-09-23 21:04:12 +0545

    omg Fetching API first time and then again Fetching another API from existing API

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -10,7 +10,19 @@ export const Pokemon = () => {
         try {
            const res = await fetch(API);
            const data = await res.json(); 
-           console.log(data);
+        //    console.log(data);
+
+            const detailedPokemonData = data.results.map( async (curPokemon) => {
+            const res = await fetch(curPokemon.url);
+            const data = await res.json();
+            return data;
+            });
+            console.log(detailedPokemonData);
+
+            const detailedResponses = await Promise.all(detailedPokemonData);
+            console.log(detailedResponses);
+            
+
         } catch (error) {
             console.log(error);
         }
